4ad5d54e4c920987d75e79948876033f578e8c58
leandroli <lizhuo99@foxmail.com>
2021-03-04 13:20:02 +0000

    create first version of Task api

---
+++ api/v1alpha1/task_types.go
@@ -0,0 +1,62 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// Step are the step of Task
+type Step corev1.Container
+
+// TaskSpec defines the desired state of Task
+type TaskSpec struct {
+	// Steps are the steps of the build
+	Steps []Step `json:"steps,omitempty"`
+
+	// Volumes is a collection of volumes that are available to mount into the
+	// steps of the build.
+	Volumes []corev1.Volume `json:"volumes,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+
+// Task is the Schema for the tasks API
+type Task struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec TaskSpec `json:"spec,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// TaskList contains a list of Task
+type TaskList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Task `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Task{}, &TaskList{})
+}
